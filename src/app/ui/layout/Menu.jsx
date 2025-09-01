d3bc4e847d9ce01ec76e0536de3ee97451ec9d5c
German Sayago <german.sayago@gmail.com>
2025-09-01 10:40:36 -0300

    se agrego el modal y el buscador

--- src/app/ui/layout/Menu.jsx
+++ src/app/ui/layout/Menu.jsx
@@ -9,12 +9,16 @@ import Link from "next/link";
 import Image from "next/image";
 import LogoMobile from "../../../../public/images/logos/logo-gobierno-white.webp";
 import LogoDesktop from "../../../../public/images/logos/logo-gobierno-slogan-white.webp";
+import SearchModal from "../commons/SearchModal";
 
 
 export default function Menu() {
   const pathname = usePathname();
   const [expanded, setExpanded] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showSearchModal, setShowSearchModal] = useState(false); // Estado para el modal
+
+
   const handleLinkClick = () => {
     setExpanded(false);
   };
@@ -23,63 +27,72 @@ export default function Menu() {
     setDropdownOpen(false);
   };
 
-  console.log(pathname);
+  const handleSearchModalShow = () => setShowSearchModal(true);
+  const handleSearchModalClose = () => setShowSearchModal(false);
+
+  //console.log(pathname);
 
 
   return (
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      sticky="top"
-      variant="dark"
-      expanded={expanded}
-      onToggle={() => setExpanded(!expanded)}
-    >
-      <Container>
-        <Navbar.Brand as={Link} href="/" onClick={handleLinkClick}>
-          <Image src={LogoMobile} className="mobile" alt="logo gobierno" />
-          <Image src={LogoDesktop} className="desktop" alt="logo gobierno" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="toggle">
-          <i className="fas fa-bars"></i>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="toggle">
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Link href="/tramites" className={`nav-link ${pathname === "/tramites" ? "active" : ""}`} onClick={handleLinkClick}>
-              Tramites
-            </Link>
-            <Link href="/areas" className={`nav-link ${pathname === "/areas" ? "active" : ""}`} onClick={handleLinkClick}>
-              Áreas
-            </Link>
-            <Link href="/noticias" className={`nav-link ${pathname === "/noticias" ? "active" : ""}`} onClick={handleLinkClick}>
-              Noticias
-            </Link>
-            <NavDropdown
-              title="Recursos"
-              show={dropdownOpen}
-              onToggle={() => setDropdownOpen(!dropdownOpen)}
-            >
-              <NavDropdown.Item as={Link} href="/mapas/" onClick={handleDropdownItemClick}>
-                Mapas
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} href="/test/" onClick={handleDropdownItemClick}>
-                Test
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/design">Design</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="https://facebook.com" target="_blank" className="">
-              <i className={`fab fa-facebook`}></i> <span className="d-md-none">Facebook</span>
-            </Nav.Link>
-            <Nav.Link href="https://instagram.com" target="_blank" className="">
-              <i className={`fab fa-instagram`}></i> <span className="d-md-none">Facebook</span>
-            </Nav.Link>
-            <Nav.Link href="https://youtube.com" target="_blank" className="">
-              <i className={`fab fa-youtube`}></i> <span className="d-md-none">Facebook</span>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        variant="dark"
+        expanded={expanded}
+        onToggle={() => setExpanded(!expanded)}
+      >
+        <Container>
+          <Navbar.Brand as={Link} href="/" onClick={handleLinkClick}>
+            <Image src={LogoMobile} className="mobile" alt="logo gobierno" />
+            <Image src={LogoDesktop} className="desktop" alt="logo gobierno" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="toggle">
+            <i className="fas fa-bars"></i>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="toggle">
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Link href="/tramites" className={`nav-link ${pathname === "/tramites" ? "active" : ""}`} onClick={handleLinkClick}>
+                Tramites
+              </Link>
+              <Link href="/areas" className={`nav-link ${pathname === "/areas" ? "active" : ""}`} onClick={handleLinkClick}>
+                Áreas
+              </Link>
+              <Link href="/noticias" className={`nav-link ${pathname === "/noticias" ? "active" : ""}`} onClick={handleLinkClick}>
+                Noticias
+              </Link>
+              <NavDropdown
+                title="Recursos"
+                show={dropdownOpen}
+                onToggle={() => setDropdownOpen(!dropdownOpen)}
+              >
+                <NavDropdown.Item as={Link} href="/mapas/" onClick={handleDropdownItemClick}>
+                  Mapas
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} href="/test/" onClick={handleDropdownItemClick}>
+                  Test
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/design">Design</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="https://facebook.com" target="_blank" className="">
+                <i className={`fab fa-fw fa-facebook`}></i> <span className="d-md-none">Facebook</span>
+              </Nav.Link>
+              <Nav.Link href="https://instagram.com" target="_blank" className="">
+                <i className={`fab fa-fw fa-instagram`}></i> <span className="d-md-none">Instagram</span>
+              </Nav.Link>
+              <Nav.Link href="https://youtube.com" target="_blank" className="">
+                <i className={`fab fa-fw fa-youtube`}></i> <span className="d-md-none">Youtube</span>
+              </Nav.Link>
+              <Nav.Link onClick={handleSearchModalShow}>
+                <i className="fa fa-fw fa-search"></i> <span className="d-md-none">Buscar</span>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <SearchModal show={showSearchModal} handleClose={handleSearchModalClose} />
+    </>
   );
 }
