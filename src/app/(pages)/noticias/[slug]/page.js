d0e473c17f3ccf91b4e55ac6f5486c5cc951f391
German Sayago <german.sayago@gmail.com>
2025-09-10 09:08:51 -0300

    realizando cambios en noticias

--- src/app/(pages)/noticias/[slug]/page.js
+++ src/app/(pages)/noticias/[slug]/page.js
@@ -1,113 +1,44 @@
-import { fetchNews, getNewsBySlug } from "@/app/lib/DataNews";
-import Link from "next/link";
+import { getNewsById, getNewsBySlug } from '@/app/lib/DataNews';
+import DetailNews from '@/app/ui/news/DetailNews';
+import RelatedNews from '@/app/ui/news/RelatedNews';
+import { notFound } from 'next/navigation';
 
-export async function generateStaticParams() {
-  const news = await fetchNews({ limit: 10 });
-  return news.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-export default async function NewsDetailPage({ params }) {
+export default async function NewsDetailPage({ params, searchParams }) {
   const { slug } = params;
+  const { id } = searchParams;
+  let detailNews = null;
 
-  //Datos de la noticia
-  const newsDetail = await getNewsBySlug(slug);
-
-  if (!newsDetail) {
-    return (
-      <div className="container my-5">
-        <h1>Noticia no encontrada</h1>
-        <p>Lo sentimos, la noticia que buscas no pudo ser encontrada.</p>
-      </div>
-    );
+  // Lógica de fetching
+  if (id) {
+    // Si la URL tiene un `id` en los searchParams, usamos ese para la consulta
+    console.log('Fetching por ID:', id);
+    detailNews = await getNewsById(id);
+  } else {
+    // Si no hay `id`, intentamos usar el `slug` como fallback (cuando la API esté lista)
+    console.log('Fetching por Slug:', slug);
+    detailNews = await getNewsBySlug(slug);
   }
 
-  const allNewsForRelated = await fetchNews({ limit: 4 });
-  const relatedNews = allNewsForRelated
-    .filter((news) => news.slug !== slug)
-    .slice(0, 3);
+  //console.log(detailNews);
+
+
+  // Si la noticia no se encuentra con ninguno de los dos métodos, mostramos 404
+  if (!detailNews) {
+    return <p>Sitio no encontrado</p>
+  }
 
   return (
-    <main data-read>
+    <main className='news news-detail' data-read>
       <div className="container my-5">
+        <span className="sr-only">Detalle de la Noticia</span>
+        <p>slug: {slug} | id: {id}</p>
         <div className="row">
-          {/* Sección de Detalle de la Noticia Principal */}
-          <section className="col-lg-8">
-            <article>
-              <span className="sr-only">Detalle de la noticia</span>
-              <h1 className="mb-4">{newsDetail.title}</h1>
-              {newsDetail.image && (
-                <>
-                  {/* eslint-disable-next-line */}{" "}
-                  <img
-                    src={newsDetail.image}
-                    alt={newsDetail.title}
-                    width={400}
-                    height={400}
-                    className="card-img-top"
-                  />{" "}
-                </>
-              )}
-              <div
-                className="mt-4"
-                dangerouslySetInnerHTML={{ __html: newsDetail.body }}
-              ></div>
-
-              <p className="text-muted mt-3">
-                Publicado el:{" "}
-                {new Date(newsDetail.publication_date).toLocaleDateString(
-                  "es-AR",
-                  {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  }
-                )}
-              </p>
-              <div
-                className="mt-4"
-                dangerouslySetInnerHTML={{ __html: newsDetail.iframe }}
-              ></div>
-            </article>
-          </section>
-
-          {/* Sección de Noticias Relacionadas */}
-          <section className="col-lg-4">
-            <div className="card my-4">
-              <h5 className="card-header">Noticias Relacionadas</h5>
-              <div className="card-body">
-                <ul className="list-unstyled mb-0">
-                  {relatedNews.length > 0 ? (
-                    relatedNews.map((news) => (
-                      <li key={news.id} className="mb-2">
-                        <Link
-                          href={`/news/${news.slug}`}
-                          className="text-decoration-none"
-                        >
-                          {news.image && (
-                            <>
-                              {/* eslint-disable-next-line */}
-                              <img
-                                src={news.image}
-                                alt={news.title}
-                                width={200}
-                                height={200}
-                                className="card-img-top"
-                              />
-                            </>
-                          )}
-                          {news.title}
-                        </Link>
-                      </li>
-                    ))
-                  ) : (
-                    <li>No hay noticias relacionadas.</li>
-                  )}
-                </ul>
-              </div>
-            </div>
-          </section>
+          <div className="col-md-8">
+            <DetailNews detailNews={detailNews} />
+          </div>
+          <div className="col-md-4">
+            <RelatedNews detailNews={detailNews} />
+          </div>
         </div>
       </div>
     </main>
