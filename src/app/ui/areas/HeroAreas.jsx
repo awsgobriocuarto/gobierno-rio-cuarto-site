31f0170e618be7ced376892277f262013b1f0e15
Salvador Coco <salvadorcocosanchez@gmail.com>
2025-09-26 11:48:43 -0300

    Estilos para areas

--- src/app/ui/areas/HeroAreas.jsx
+++ src/app/ui/areas/HeroAreas.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import HeaderSection from "../layout/HeaderSection";
 
 export default function HeroAreas({ area }) {
   if (!area) {
@@ -26,10 +27,14 @@ export default function HeroAreas({ area }) {
   return (
     <section className="area-hero">
       <div className="container">
-        <h2>{area.name || "Área sin nombre"}</h2>
-        <p className="lead mb-4">
-          {area.propouse || "No hay descripción o propósito."}
-        </p>
+        <h1>{area.name || "Área sin nombre"}</h1>
+        <p
+          className="mb-4"
+          dangerouslySetInnerHTML={{
+            __html: area.propouse || "No hay descripción o propósito.",
+          }}
+        ></p>
+
         <h4 className="mb-3">Información de Contacto</h4>
 
         <div className="info-list">
@@ -38,8 +43,11 @@ export default function HeroAreas({ area }) {
             const icon = ICONS[type] || null;
 
             return (
-              <div className="info-item mb-3" key={type}>
-                {icon && <span className="me-2">{icon}</span>}
+              <div
+                className="p-2 border-start border-3 border-primary bg-light"
+                key={type}
+              >
+                {icon && <span className="me-2 text-primary">{icon}</span>}
                 <span>{formattedValues}</span>
               </div>
             );
