21999305fa6a17651020563c74186afc09e01b01
Alejo Mendizabal <alejomendi24@gmail.com>
2026-01-29 11:27:39 -0300

    fix ante titulo icons

--- src/app/ui/layout/HeaderSection.jsx
+++ src/app/ui/layout/HeaderSection.jsx
@@ -3,30 +3,26 @@ import ListIcons from "../icons/ListIcons";
 const LIST_OF_ICONS = [
   { name: "circles", color: "yellow", size: "30" },
   { name: "squares", color: "pink", size: "30" },
-  {
-    name: "waves",
-    color: "lightblue",
-    size: "30",
-  },
+  { name: "waves", color: "lightblue", size: "30" },
 ];
 
 export default function HeaderSection({
   title = "Titulo de la Seccion",
   subtitle = "",
-  border = false
+  border = false,
 }) {
   return (
     <div className={`header ${border ? "header-border" : ""}`}>
       <div className="header-icons">
-        <ListIcons icons={LIST_OF_ICONS}></ListIcons>
+        <ListIcons icons={LIST_OF_ICONS} />
       </div>
+
       <div className="header-text">
         <h2>{title}</h2>
-        {subtitle ? <p>{subtitle}</p> : ""}
+        {subtitle && <p>{subtitle}</p>}
       </div>
-      <div className="header-form">
 
-      </div>
+      <div className="header-form"></div>
     </div>
   );
 }
