a8b6512124948f8ed666b0df97db0ac6df642421
Salvador Coco <salvadorcocosanchez@gmail.com>
2026-04-13 12:36:17 -0300

    baje el boton de wsp

--- src/app/ui/commons/ShareSocial.jsx
+++ src/app/ui/commons/ShareSocial.jsx
@@ -12,7 +12,7 @@ export default function ShareSocial({ title = "", text = "" }) {
 
   const encodedUrl = encodeURIComponent(currentUrl);
   const encodedShareText = encodeURIComponent(
-    title ? `${title} ${currentUrl}` : currentUrl
+    title ? `${title} ${currentUrl}` : currentUrl,
   );
 
   const fallbackCopy = () => {
@@ -64,19 +64,36 @@ export default function ShareSocial({ title = "", text = "" }) {
 
   return (
     <div className="share-social">
-      <button 
-        onClick={handleShare} 
-        className="btn-share" 
+      <button
+        onClick={handleShare}
+        className="btn-share"
         title="Compartir esta noticia"
       >
-        <i className={`fa-solid ${copySuccess ? "fa-check" : "fa-share-nodes"}`}></i>
+        <i
+          className={`fa-solid ${copySuccess ? "fa-check" : "fa-share-nodes"}`}
+        ></i>
         {copySuccess ? "¡Enlace copiado!" : "Compartir"}
       </button>
 
       <div className="social-links">
-        <a rel="noopener " href="https://www.facebook.com/gobderiocuarto" className="btn-social" target="_blank" aria-label="Facebook Municipalidad"><i className={`fa-brands fa-facebook-f`}></i></a>
-        <a rel="noopener noreferrer" href="https://twitter.com/gobderiocuarto" className="btn-social" target="_blank" aria-label="Twitter Municipalidad"><i className={`fa-brands fa-twitter`}></i></a>
-        <a rel="noopener noreferrer" href="https://wa.me/543584397000" className="btn-social" target="_blank" aria-label="WhatsApp Municipalidad"><i className={`fa-brands fa-whatsapp`}></i></a>
+        <a
+          rel="noopener "
+          href="https://www.facebook.com/gobderiocuarto"
+          className="btn-social"
+          target="_blank"
+          aria-label="Facebook Municipalidad"
+        >
+          <i className={`fa-brands fa-facebook-f`}></i>
+        </a>
+        <a
+          rel="noopener noreferrer"
+          href="https://twitter.com/gobderiocuarto"
+          className="btn-social"
+          target="_blank"
+          aria-label="Twitter Municipalidad"
+        >
+          <i className={`fa-brands fa-twitter`}></i>
+        </a>
       </div>
     </div>
   );
