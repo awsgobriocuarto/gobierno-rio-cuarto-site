2ea9db0a10179bc5e97995af5a71d74449ebdd29
Salvador Coco <salvadorcocosanchez@gmail.com>
2026-04-13 08:52:50 -0300

    Baje el num del compartir.

--- src/app/ui/commons/ShareSocial.jsx
+++ src/app/ui/commons/ShareSocial.jsx
@@ -20,18 +20,33 @@ export default function ShareSocial({ title = "", text = "" }) {
 
   return (
     <div className="share-social">
-      <button 
-        onClick={handleShare} 
-        className="btn-share" 
+      <button
+        onClick={handleShare}
+        className="btn-share"
         title="Compartir esta noticia"
       >
         <i className="fa-solid fa-share-nodes"></i> Compartir
       </button>
 
       <div className="social-links">
-        <a rel="noopener " href="https://www.facebook.com/gobderiocuarto" className="btn-social" target="_blank" aria-label="Facebook Municipalidad"><i className={`fa-brands fa-facebook-f`}></i></a>
-        <a rel="noopener noreferrer" href="https://twitter.com/gobderiocuarto" className="btn-social" target="_blank" aria-label="Twitter Municipalidad"><i className={`fa-brands fa-twitter`}></i></a>
-        <a rel="noopener noreferrer" href="https://wa.me/543584397000" className="btn-social" target="_blank" aria-label="WhatsApp Municipalidad"><i className={`fa-brands fa-whatsapp`}></i></a>
+        <a
+          rel="noopener "
+          href="https://www.facebook.com/gobderiocuarto"
+          className="btn-social"
+          target="_blank"
+          aria-label="Facebook Municipalidad"
+        >
+          <i className={`fa-brands fa-facebook-f`}></i>
+        </a>
+        <a
+          rel="noopener noreferrer"
+          href="https://twitter.com/gobderiocuarto"
+          className="btn-social"
+          target="_blank"
+          aria-label="Twitter Municipalidad"
+        >
+          <i className={`fa-brands fa-twitter`}></i>
+        </a>
       </div>
     </div>
   );
